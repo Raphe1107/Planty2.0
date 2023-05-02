d3dcb54b206cac6954b8087c9e17e788da500fe2
Raphe1107 <rnuguet@gmail.com>
2023-05-02 18:16:23 +0200

    update

--- wp-content/themes/astra-enfant/functions.php
+++ wp-content/themes/astra-enfant/functions.php
@@ -1,6 +1,17 @@
 <?php
-    add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_styles' );
-    function astra_child_enqueue_styles() {
+    add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+    function theme_enqueue_styles() {
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
     }
+
+    function astra_enfant_menus (){
+        register_nav_menu ('header', 'En tête menu');
+        register_nav_menu ('footer', 'Pied de page');
+    }
+
+    add_action ( 'after_setup_theme', 'astra_enfant_menus' );
+
+   
+    
 ?>
